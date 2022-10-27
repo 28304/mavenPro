8a2388c5bde4b76069c9c906abba7efe67fd5d9b
hzh <2830433167@qq.com>
2022-10-27 21:05:26 +0800

    gitTest-masterV2

--- maven/mavenPro/src/main/java/com/exer/maven/App.java
+++ maven/mavenPro/src/main/java/com/exer/maven/App.java
@@ -14,5 +14,6 @@ public class App
         System.out.println("aaaaaaaaaaaa");
         System.out.println("aaaaaaaaaaaa");
         System.out.println("aaaaaaaaaaaa");
+        System.out.println("master test");
     }
 }
